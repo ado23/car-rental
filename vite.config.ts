43793492d006358ad5062b7e70e4ebf4b2ab0e7e
Admir <28600628+ado23@users.noreply.github.com>
2024-05-21 23:37:14 +0200

    Added eslint and prettier configs.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, UserConfig } from "vite";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
 
   server: {
     port: 3000,
     open: true,
-  },
-});
+    watch: {
+      usePolling: true
+    }
+  }
+} as UserConfig);
